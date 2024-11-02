7ad2875bdf165c1071013cbf9efe6be4c2b3b0be
abdulmajid18 <nuhuabdulmajid58@gmail.com>
2024-11-02 01:05:54 +0000

    start thread using lambda

--- chapter-1/work_callable.cpp
+++ chapter-1/work_callable.cpp
@@ -37,3 +37,17 @@ std::thread my_thread{background_task()};
 // The intended functionality (concurrent execution) will not take place, but there will be no immediate errors unless you attempt to use my_thread in a manner appropriate for a thread
 std::thread my_thread(background_task());
 
+void do_something() {
+        std::cout << "Doing some work in the background...\n";
+    }
+
+void do_something_else() {
+    std::cout << "Doing something else in the background...\n";
+}
+
+std::thread my_threads([] {
+    do_something();
+    do_something_else();
+});
+
+ 
